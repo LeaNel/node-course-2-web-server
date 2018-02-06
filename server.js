27da61a685149780bd035fc57c3ffb471cfbd922
Leanne Nel <leanne@Leannes-MacBook-Air.local>
2018-02-06 10:19:06 +0200

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -57,6 +59,6 @@ app.get('/bad', (req,res)=> {
         errorMessage: 'This page could not be rendered'
     });
 })
-app.listen(3000, ()=> {
-    console.log('Server is up on Port 3000');
+app.listen(port, ()=> {
+    console.log(`Server is up on Port ${port}`);
 });
